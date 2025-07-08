3dadd9b9d661eed599bab0a24e19403010286061
Arvindh-1509 <72424948+Arvindh1509@users.noreply.github.com>
2025-07-09 03:23:16 +0530

    With Teachers Info component

--- smproject/src/axios.js
+++ smproject/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:`${window.location.origin}/vinterbash`
+    // baseURL:`${window.location.origin}/vinterbash`
+    baseURL:"http://localhost:8000"
 })
 
 export default instance;
