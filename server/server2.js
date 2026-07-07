d0011ee098442701fe3ec48606b9406796d8cdf8
Arvindh-1509 <rockstararvindh@gmail.com>
2026-07-07 14:12:50 +0530

    commit for allowing vercel

--- server/server2.js
+++ server/server2.js
@@ -13,11 +13,13 @@ const PORT = process.env.PORT || 8000;
 app.use(cors({
     origin: [
     //     "http://ec2-184-73-128-194.compute-1.amazonaws.com",
-    "https://vinter2-0.onrender.com/vinterbash","http://localhost:3001"
+    "https://vinter2-0.onrender.com/vinterbash","http://localhost:3001",
     //     "http://ec2-184-73-128-194.compute-1.amazonaws.com:3000",
-    "https://vinter2-0.onrender.com/vinterbash"
+    "https://vinter2-0.onrender.com/vinterbash",
+    "https://vinter2-0-xtdb.vercel.app",
     //     "http://vinterbash.in:3000", "vinterbash.in:3000",
-    //     "https://vinterbash.in", "http://vinterbash.in", "http://localhost:3000/"
+        "https://vinterbash.in"
+    // , "http://vinterbash.in", "http://localhost:3000/"
     ]
 }));
 app.use(express.json());
