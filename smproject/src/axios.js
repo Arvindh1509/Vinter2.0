31eeb915b31b31da585458efc0715ef75228596c
R <jayar@deloitte.com>
2026-07-14 00:51:18 +0530

    Updated base url

--- smproject/src/axios.js
+++ smproject/src/axios.js
@@ -4,7 +4,7 @@ const instance = axios.create({
     // baseURL:`${window.location.origin}/vinterbash`
     // baseURL:"http://localhost:8000"
     // baseURL:"https://vinter2-0.onrender.com/"
-    baseURL:"http://vinterbash-alb-1429442373.us-east-1.elb.amazonaws.com/"
+    baseURL:"https://vinterbash.co.in/vinterbash"
     
 })
 
