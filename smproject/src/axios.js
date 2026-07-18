26854505c5658087fa3e0e1065537e3afa6fe402
Arvindh-1509 <rockstararvindh@gmail.com>
2026-07-18 10:59:15 +0530

    axios change

--- smproject/src/axios.js
+++ smproject/src/axios.js
@@ -4,7 +4,8 @@ const instance = axios.create({
     // baseURL:`${window.location.origin}/vinterbash`
     // baseURL:"http://localhost:8000"
     // baseURL:"https://vinter2-0.onrender.com/"
-    baseURL:"https://vinterbash.co.in/"
+    // baseURL:"https://vinterbash.co.in/"
+    baseURL: `${window.location.origin}`
     
 })
 
