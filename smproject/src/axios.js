9ca6cb3d6707dc9374aa6a66db008fdafc955561
Arvindh-1509 <72424948+Arvindh1509@users.noreply.github.com>
2026-07-20 01:05:17 +0530

    Update axios baseURL for API requests

--- smproject/src/axios.js
+++ smproject/src/axios.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const instance = axios.create({
     // baseURL:`${window.location.origin}/vinterbash`
     // baseURL:"http://localhost:8000"
-    // baseURL:"https://vinter2-0.onrender.com/"
-     baseURL:"https://vinterbash.co.in/"
+    baseURL:"https://vinter2-0.onrender.com/"
+     // baseURL:"https://vinterbash.co.in/"
     //baseURL: `${window.location.origin}`
     
 })
